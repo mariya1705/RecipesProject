86b6f1d6e76444f26b16864a424898f600efd470
Mariya Fareed <mariyafareed@Mariyas-Air.Dlink>
2021-03-31 21:52:31 +0530

    About Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,7 +50,7 @@ const Navbar = () => {
             About
           </Link>
           <div className="nav-link contact-link">
-            <Link to="/about" className="btn" onClick={() => setShow(false)} s>
+            <Link to="/contact" className="btn" onClick={() => setShow(false)}>
               Contact
             </Link>
           </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,39 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
-const about = () => {
+import { StaticImage } from "gatsby-plugin-image"
+
+const About = () => {
   return (
     <Layout>
-      <h1>hello from about page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person sprinkling salt"
+            placeholder="blurred"
+            className="about-img"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
